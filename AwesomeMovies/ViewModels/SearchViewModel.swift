935e797430d21633f1e5524e1e3e3324c474582a
Tomás Pacheco <tomas.pacheco.c@gmail.com>
2026-01-14 13:48:23 -0300

    more general validations , navigation in search view, image refactor, no image validation

--- AwesomeMovies/ViewModels/SearchViewModel.swift
+++ AwesomeMovies/ViewModels/SearchViewModel.swift
@@ -9,24 +9,42 @@ import Foundation
 
 @Observable
 class SearchViewModel {
+    
+    enum FetchStatus {
+        case notStarted
+        case fetching
+        case success
+        case failed(underlyingError: Error)
+    }
+    
+    private(set) var searchFetchStatus: FetchStatus = .notStarted
     private(set) var errorMessage: String?
     private(set) var searchTitles: [Title] = []
     private let dataFetcher = DataFetcher()
     
     func getSearchTitles(by media: String, for title: String) async{
+		
+		if(!searchTitles.isEmpty && title.isEmpty){
+			return
+		}
+		
+        searchFetchStatus = .fetching
         do {
             errorMessage = nil
             
-            if(searchTitles.isEmpty){
+            if(searchTitles.isEmpty || title.isEmpty){
+				print("Is empty")
                 searchTitles = try await dataFetcher.fetchTitles(for: media, by: "trending")
             } else {
+				print("Is not empty")
                 searchTitles = try await dataFetcher.fetchTitles(for: media, by: "search", with: title)
             }
-            
-            print(searchTitles)
+			
+            searchFetchStatus = .success
         } catch {
             print("Error searching titles: \(error)")
             errorMessage = error.localizedDescription
+            searchFetchStatus = .failed(underlyingError: error)
         }
     }
 }
--- AwesomeMovies/Views/SearchView.swift
+++ AwesomeMovies/Views/SearchView.swift
@@ -12,55 +12,50 @@ struct SearchView: View {
     //var titles = Title.previewTitles
     @State private var searchByMovie = true
     @State private var searchString = ""
+    @State private var navigationPath = NavigationPath()
     private var searchViewModel = SearchViewModel()
 
     var body: some View {
-        NavigationStack {
-            ScrollView {
-                LazyVGrid(columns: [GridItem(), GridItem(), GridItem()]) {
-                    ForEach(searchViewModel.searchTitles) { title in
-                        AsyncImage(url: URL(string: title.posterPath ?? "")) {
-                            image in
-                            image
-                                .resizable()
-                                .scaledToFit()
-                                .clipShape(.rect(cornerRadius: 15))
-                        } placeholder: {
-                            ProgressView()
-                        }
-                        .frame(width: 120, height: 200)
+        NavigationStack(path: $navigationPath) {
+            GeometryReader { geo in
+                ScrollView {
+
+                    switch searchViewModel.searchFetchStatus {
+                    case .notStarted:
+                        EmptyView()
+                    case .fetching:
+                        ProgressView()
+                            // With this geo sizing, we center the elements in the available screen
+                            .frame(width: geo.size.width, height: geo.size.height)
+                    case .success:
+                        titlesGrid(geometry: geo)
+                    case .failed(_):
+                        errorMessage(geometry: geo)
                     }
+
                 }
             }
-            .navigationTitle(
-                searchByMovie
-                    ? Constants.movieSearchString : Constants.tvSearchString
-            )
+            .navigationTitle(searchByMovie ? Constants.movieSearchString : Constants.tvSearchString)
             .toolbar {
                 ToolbarItem(placement: .topBarTrailing) {
                     Button {
                         searchByMovie.toggle()
-                        
+
                         Task {
-                            await searchViewModel.getSearchTitles(
-                                by: searchByMovie ? "movie" : "tv", for: searchString)
+                            await searchViewModel.getSearchTitles(by: searchByMovie ? "movie" : "tv", for: searchString)
                         }
-                        
+
                     } label: {
                         Image(
-                            systemName: searchByMovie
-                                ? Constants.movieIconString
-                                : Constants.tvIconString)
+                            systemName: searchByMovie ? Constants.movieIconString : Constants.tvIconString)
                     }
                 }
             }.searchable(
                 text: $searchString,
-                prompt: searchByMovie
-                    ? Constants.movieSearchPlaceholderString
-                    : Constants.tvSearchPlaceholderString
+                prompt: searchByMovie ? Constants.movieSearchPlaceholderString : Constants.tvSearchPlaceholderString
             )
             .task(id: searchString) {
-                try? await Task.sleep(for: .milliseconds(2000))
+                try? await Task.sleep(for: .milliseconds(500))
                 print("Searching....")
 
                 if Task.isCancelled {
@@ -68,9 +63,72 @@ struct SearchView: View {
                     return
                 }
 
-                await searchViewModel.getSearchTitles(
-                    by: searchByMovie ? "movie" : "tv", for: searchString)
+                await searchViewModel.getSearchTitles(by: searchByMovie ? "movie" : "tv", for: searchString)
             }
+            .navigationDestination(for: Title.self) { title in
+                TitlesDetailVew(title: title)
+            }
+        }
+    }
+
+    @ViewBuilder
+    private func titlesGrid(geometry: GeometryProxy) -> some View {
+        LazyVGrid(columns: [GridItem(), GridItem(), GridItem()]) {
+            ForEach(searchViewModel.searchTitles) { title in
+                titleCell(for: title)
+            }
+        }
+    }
+
+    @ViewBuilder
+    private func titleCell(for title: Title) -> some View {
+        Group {
+            if let url = URL(string: title.posterPath ?? "") {
+				
+                AsyncImage(url: url) { phase in
+                    if let image = phase.image {
+                        image
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(.rect(cornerRadius: 15))
+                    } else {
+                        ProgressView()
+                    }
+                }
+            } else {
+				VStack{
+					Image(systemName: "photo.fill")
+						.resizable()
+						.scaledToFit()
+						.padding(10)
+						.foregroundColor(.gray)
+					Text(title.title ?? "Sin titulo")
+						.bold()
+						.font(.system(size: 10))
+						.multilineTextAlignment(.center)
+				}
+				.padding(10)
+            }
+        }
+        .frame(width: 120, height: 200)
+        .onTapGesture {
+            navigationPath.append(title)
+        }
+    }
+
+    @ViewBuilder
+    private func errorMessage(geometry: GeometryProxy)
+        -> some View
+    {
+        if let error = searchViewModel.errorMessage {
+            Text(error)
+                .foregroundStyle(.red)
+                .padding()
+                .background(.ultraThinMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                .frame(
+                    width: geometry.size.width,
+                    height: geometry.size.height)
         }
     }
 }

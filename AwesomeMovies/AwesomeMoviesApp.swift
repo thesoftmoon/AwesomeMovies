1a16b96d2d609acfac11bdf4c76d40ab94a38839
Tomás Pacheco <tomas.pacheco.c@gmail.com>
2026-01-23 16:27:55 -0300

    SwiftData Title model implementation start

--- AwesomeMovies/AwesomeMoviesApp.swift
+++ AwesomeMovies/AwesomeMoviesApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct AwesomeMoviesApp: App {
@@ -16,6 +17,7 @@ struct AwesomeMoviesApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-        }
+		}
+		.modelContainer(for: Title.self)
     }
 }
--- AwesomeMovies/Models/Title.swift
+++ AwesomeMovies/Models/Title.swift
@@ -5,7 +5,7 @@
 //  Created by Tomás  Pacheco on 20-11-25.
 //
 
-import Foundation
+import SwiftData
 
 struct TMDBAPIObject: Decodable {
     var results: [Title] = []
@@ -13,13 +13,41 @@ struct TMDBAPIObject: Decodable {
 
 // Decodable allows swift to create the object from data source and identifiable get the id from it
 // Hashable let Swift identify if the title are the same, like an ID
-struct Title: Decodable, Identifiable, Hashable {
+@Model
+class Title: Decodable, Identifiable, Hashable {
     var id: Int?
     // The ' is for optional properties
     var title: String?
     var name: String?
     var overview: String?
     var posterPath: String?
+	
+	init(id: Int? = nil, title: String? = nil, name: String? = nil, overview: String? = nil, posterPath: String? = nil) {
+		self.id = id
+		self.title = title
+		self.name = name
+		self.overview = overview
+		self.posterPath = posterPath
+	}
+	
+	enum CodingKeys: CodingKey {
+		case id
+		case title
+		case name
+		case overview
+		case posterPath
+	}
+	
+	required init(from decoder: any Decoder) throws {
+		//	this line creates a container to allows acces
+		//	to JSON data using the keys defined in CodingKeys
+		let container = try decoder.container(keyedBy: CodingKeys.self)
+		id = try container.decodeIfPresent(Int.self, forKey: .id)
+		title = try container.decodeIfPresent(String.self, forKey: .title)
+		name = try container.decodeIfPresent(String.self, forKey: .name)
+		overview = try container.decodeIfPresent(String.self, forKey: .overview)
+		posterPath = try container.decodeIfPresent(String.self, forKey: .posterPath)
+	}
     
     static var previewTitles = [
         Title(
